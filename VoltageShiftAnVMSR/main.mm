9e271bc716ff42e0ea868ef55d2939fdeaaaecfa
Ahmad Ansori Palembani <palembani@gmail.com>
2024-07-14 02:57:21 +0700

    fix: Escape quote doesn't actually escaping it

--- VoltageShiftAnVMSR/main.mm
+++ VoltageShiftAnVMSR/main.mm
@@ -1514,10 +1514,10 @@ void writeLaunchDaemons(std::vector<int>  values = {0},int min = 160  ) {
     }
     
     
-   output << "sudo echo \""
-   << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-   << "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
-   <<  "<plist version=\"1.0\">"
+   output << "sudo echo \\\""
+   << "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>"
+   << "<!DOCTYPE plist PUBLIC \\\"-//Apple//DTD PLIST 1.0//EN\\\" \\\"http://www.apple.com/DTDs/PropertyList-1.0.dtd\\\">"
+   <<  "<plist version=\\\"1.0\\\">"
    << "<dict>"
     << "<key>RunAtLoad</key><true/>"
    << "<key>Label</key>"
